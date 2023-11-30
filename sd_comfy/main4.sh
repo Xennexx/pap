7e3699637ba3ef968d78dce755e9db398178f9bb
Xennexx <87284172+Xennexx@users.noreply.github.com>
2023-11-30 02:51:22 -0700

    Add files via upload

---
+++ sd_comfy/main4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+current_dir=$(dirname "$(realpath "$0")")
+cd $current_dir
+source .env
+
+# Set up a trap to call the error_exit function on ERR signal
+trap 'error_exit "### ERROR ###"' ERR
+
+
+if [[ -z "$INSTALL_ONLY" ]]; then
+  echo "### Starting Stable Diffusion Comfy ###"
+  log "Starting Stable Diffusion Comfy"
+  cd "$REPO_DIR"
+  PYTHONUNBUFFERED=1 service_loop "python main.py --dont-print-server --highvram --port 7102" > $LOG_DIR/sd_comfy_2.log 2>&1 &
+  echo $! > /tmp/sd_comfy.pid
+fi
+
+
+
+if env | grep -q "PAPERSPACE"; then
+  echo "Link: https://$PAPERSPACE_FQDN/com4/"
+fi
+
+
+echo "### Done ###"
---
+++ sd_comfy/main5.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+current_dir=$(dirname "$(realpath "$0")")
+cd $current_dir
+source .env
+
+# Set up a trap to call the error_exit function on ERR signal
+trap 'error_exit "### ERROR ###"' ERR
+
+if [[ -z "$INSTALL_ONLY" ]]; then
+  echo "### Starting Stable Diffusion Comfy ###"
+  log "Starting Stable Diffusion Comfy"
+  cd "$REPO_DIR"
+  PYTHONUNBUFFERED=1 service_loop "python main.py --dont-print-server --highvram --port 7103" > $LOG_DIR/sd_comfy_2.log 2>&1 &
+  echo $! > /tmp/sd_comfy.pid
+fi
+
+if env | grep -q "PAPERSPACE"; then
+  echo "Link: https://$PAPERSPACE_FQDN/com5/"
+fi
+
+echo "### Done ###"
